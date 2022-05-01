f8be86522657469a6cc5986b37f8e3edadfe7c59
bbzekovich <bzekovich@gmail.com>
2022-05-01 20:08:56 +0400

    newchanges

---
+++ amocana3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace amocana_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 6;
+            int ls = 5;
+
+            // სამწუხაროდ ვერ გავეკეთე 
+
+        }
+    }
+}
